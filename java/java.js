9b86cf9f23dbaf8ff1b7696fa02db7d6535d86c8
Tyler Nichols <tegulator98@gmail.com>
2019-06-23 14:43:58 -0600

    Firebase added and trying to fix the firebase ref.

--- java/java.js
+++ java/java.js
@@ -1,4 +1,5 @@
-var firebaseConfig = {
+var trainData = "Crazy Train";
+var Config = {
     apiKey: "AIzaSyDbN8sSt30U8DtLv4JHKsMDSj70X4P1yYo",
     authDomain: "trains-d65a0.firebaseapp.com",
     databaseURL: "https://trains-d65a0.firebaseio.com",
@@ -7,7 +8,103 @@ var firebaseConfig = {
     messagingSenderId: "770798326085",
     appId: "1:770798326085:web:2d420720f1c0e8f1"
   };
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(Config);
   
   var trains = firebase.database();
+ 
+$("#train-table").append(trainData);
+  trains.ref().push(trainData);
+ 
+$("#add-train-btn").on("click", function(event) {
+    // Prevent the default form submit behavior
+    event.preventDefault();
+  
+    // Grab user input
+    var trainName = $("#train-name-input")
+      .val()
+      .trim();
+    var destination = $("#destination-input")
+      .val()
+      .trim();
+    var firstTrain = $("#first-train-input")
+      .val()
+      .trim();
+    var frequency = $("#frequency-input")
+      .val()
+      .trim();
+  
+    // Create object for holding new train
+    var newTrain = {
+      name: trainName,
+      destination: destination,
+      firstTrain: firstTrain,
+      frequency: frequency
+    };
+  
+    // Upload train data
+    trainData.ref().push(newTrain);
+  
+    // Log it all
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTrain);
+    console.log(newTrain.frequency);
+  
+    // Tell the user it worked
+    alert("Train successfully added");
+  
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-train-input").val("");
+    $("#frequency-input").val("");
+  });
+  
+  // Create Firebase event that adds trains to the database in a row in the html when a user adds an entry
+  trainData.ref().on("child_added", function(childSnapshot, prevChildKey) {
+    console.log(childSnapshot.val());
+  
+    // Store everything into a variable.
+    var tName = childSnapshot.val().name;
+    var tDestination = childSnapshot.val().destination;
+    var tFrequency = childSnapshot.val().frequency;
+    var tFirstTrain = childSnapshot.val().firstTrain;
+  
+    var timeArr = tFirstTrain.split(":");
+    var trainTime = moment()
+      .hours(timeArr[0])
+      .minutes(timeArr[1]);
+    var maxMoment = moment.max(moment(), trainTime);
+    var tMinutes;
+    var tArrival;
+  
+    // If the first train is later than the current time, sent arrival to the first train time
+    if (maxMoment === trainTime) {
+      tArrival = trainTime.format("hh:mm A");
+      tMinutes = trainTime.diff(moment(), "minutes");
+    } else {
+      // figure out the minutes until arrival using math
+      var differenceTimes = moment().diff(trainTime, "minutes");
+      var tRemainder = differenceTimes % tFrequency;
+      tMinutes = tFrequency - tRemainder;
+      
+      tArrival = moment()
+        .add(tMinutes, "m")
+        .format("hh:mm A");
+    }
+    console.log("tMinutes:", tMinutes);
+    console.log("tArrival:", tArrival);
+  
+    // Add each train's data into the table
+    $("#train-table > tbody").append(
+      $("<tr>").append(
+        $("<td>").text(tName),
+        $("<td>").text(tDestination),
+        $("<td>").text(tFrequency),
+        $("<td>").text(tArrival),
+        $("<td>").text(tMinutes)
+      )
+    );
+  });
+  
   
